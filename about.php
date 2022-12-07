380b701cb60e34a5115e896847175ed193d651c0
YangaBonga <120003196+YangaBonga@users.noreply.github.com>
2022-12-07 08:59:57 +0200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,176 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+<?php include 'components/user_header.php'; ?>
+<!-- header section ends -->
+
+<div class="heading">
+   <h3>about us</h3>
+   <p><a href="home.php">home</a> <span> / about</span></p>
+</div>
+
+<!-- about section starts  -->
+
+<section class="about">
+
+   <div class="row">
+
+      <div class="image">
+         <img src="images/Medical prescription-amico.svg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>Why Choose Us?</h3>
+         <p>It’s our attention to the small stuff, scheduling of timelines 
+            and keen delivery management that makes us stand out from the rest. As Phila-sizwe pharmarcy, we are creative, reliable, safe
+            while keeping a close eye on the calendar and your budget! Our prices are competitive and fair. There are no surprise bills.</p>
+         <a href="menu.php" class="btn">our products</a>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- steps section starts  -->
+
+<section class="steps">
+
+   <h1 class="title">simple steps</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/choose.webp" alt="">
+         <h3>choose order</h3>
+         <p>These days you can find almost anything on the Internet.</p>
+      </div>
+
+      <div class="box">
+         <img src="images/delivery.webp" alt="">
+         <h3>fast delivery</h3>
+         <p>Delivery is FREE to an address of your choice for orders.</p>
+      </div>
+
+      <div class="box">
+         <img src="images/pharma.jpg" alt="">
+         <h3>enjoy services</h3>
+         <p>Enjoy our services with one of the best Pharmacists</p>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- steps section ends -->
+
+<!-- reviews section starts  -->
+
+<section class="reviews">
+
+   <h1 class="title">customer's reivews</h1>
+
+   <div class="swiper reviews-slider">
+
+   
+               <a href="index.php" class="btn">View Reviews</a>
+
+      <div class="swiper-pagination"></div>
+
+   </div>
+
+</section>
+
+<!-- reviews section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+<?php include 'components/footer.php'; ?>
+<!-- footer section ends -->=
+
+
+
+
+
+
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+<script>
+
+var swiper = new Swiper(".reviews-slider", {
+   loop:true,
+   grabCursor: true,
+   spaceBetween: 20,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+   },
+   breakpoints: {
+      0: {
+      slidesPerView: 1,
+      },
+      700: {
+      slidesPerView: 2,
+      },
+      1024: {
+      slidesPerView: 3,
+      },
+   },
+});
+
+</script>
+
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,205 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+include 'components/add_cart.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home</title>
+
+   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.2/js/bootstrap.min.js" />
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/react/18.2.0/umd/react.production.min.js" />
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css"> 
+
+</head>
+<body>
+
+<?php include 'components/user_header.php'; ?>
+
+
+
+<section class="hero">
+
+   <div class="swiper hero-slider"> 
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide">
+            <div class="content">
+               <span>order online</span>
+               <h3>View Our Products</h3>
+               <a href="menu.php" class="btn">see products</a>
+            </div>
+            <div class="image">
+               <img src="images/slide1.jpg" alt="">
+            </div>
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="content">
+               <span>order online</span>
+               <h3>Full Check Up</h3>
+               <a href="menu.php" class="btn">View Our Products</a>
+            </div>
+            <div class="image">
+               <img src="images/slide2.jpg" alt="">
+            </div>
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="content">
+               <span>order online</span>
+               <h3>Best Medication Prices</h3>
+               <a href="menu.php" class="btn">View Our Products</a>
+            </div>
+            <div class="image">
+               <img src="images/slide3.jpg" alt="">
+            </div>
+         </div>
+
+      </div>
+
+      <div class="swiper-pagination"></div>
+
+   </div>
+
+</section>
+
+<section class="category">
+
+   <h1 class="title">medicine category</h1>
+
+   <div class="box-container">
+
+      <a href="category.php?category=Pain Relief" class="box">
+         <img src="images/cat1.png" alt="">
+         <h3>Pain Relief</h3>
+      </a>
+
+      <a href="category.php?category=Vitamins" class="box">
+         <img src="images/cat2.png" alt="">
+         <h3>Vitamins</h3>
+      </a>
+
+      <a href="category.php?category=Injections" class="box">
+         <img src="images/cat3.png" alt="">
+         <h3>Injections</h3>
+      </a>
+
+      <a href="category.php?category=Scripted Medication" class="box">
+         <img src="images/cat4.png" alt="">
+         <h3>Chronic Medication</h3>
+      </a>
+
+   </div>
+
+</section>
+
+
+
+
+<section class="products">
+
+   <h1 class="title">latest Medicine</h1>
+
+   <div class="box-container">
+
+      <?php
+         $select_products = $conn->prepare("SELECT * FROM `products` LIMIT 6");
+         $select_products->execute();
+         if($select_products->rowCount() > 0){
+            while($fetch_products = $select_products->fetch(PDO::FETCH_ASSOC)){
+      ?>
+      <form action="" method="post" class="box">
+         <input type="hidden" name="pid" value="<?= $fetch_products['id']; ?>">
+         <input type="hidden" name="name" value="<?= $fetch_products['name']; ?>">
+         <input type="hidden" name="price" value="<?= $fetch_products['price']; ?>">
+         <input type="hidden" name="image" value="<?= $fetch_products['image']; ?>">
+         <a href="quick_view.php?pid=<?= $fetch_products['id']; ?>" class="fas fa-eye"></a>
+         <button type="submit" class="fas fa-shopping-cart" name="add_to_cart"></button>
+         <img src="uploaded_img/<?= $fetch_products['image']; ?>" alt="">
+         <a href="category.php?category=<?= $fetch_products['category']; ?>" class="cat"><?= $fetch_products['category']; ?></a>
+         <div class="name"><?= $fetch_products['name']; ?></div>
+         <div class="flex">
+            <div class="price"><span>R</span><?= $fetch_products['price']; ?></div>
+            <input type="number" name="qty" class="qty" min="1" max="99" value="1" maxlength="2">
+         </div>
+      </form>
+      <?php
+            }
+         }else{
+            echo '<p class="empty">no products added yet!</p>';
+         }
+      ?>
+
+   </div>
+
+   <div class="more-btn">
+      <a href="menu.php" class="btn">veiw all</a>
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>
+
+
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+<script>
+
+var swiper = new Swiper(".hero-slider", {
+   loop:true,
+   grabCursor: true,
+   effect: "flip",
+   pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+   },
+});
+
+</script>
+
+</body>
+</html>
